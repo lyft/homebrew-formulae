b8b007a4894bfe9d9a2780fdb7f0c52a72991f40
Keith Smiley <keithbsmiley@gmail.com>
2015-08-05 16:11:50 -0700

    Version swiftlint fork

--- Formula/swiftlint.rb
+++ Formula/swiftlint.rb
@@ -1,6 +1,7 @@
 class Swiftlint < Formula
   desc "Experimental tool to enforce Swift style and conventions"
   homepage "https://github.com/lyft/SwiftLint"
+  url "https://github.com/lyft/SwiftLint.git", :tag => "1.0.0", :revision => "2214eaa85ea1548e47d2f0a6fe93d74abd3f3c2c"
   head "https://github.com/lyft/SwiftLint.git", :branch => "update-rules"
 
   depends_on :xcode => ["6.3", :build]
