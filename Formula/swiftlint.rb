8e8ab4aa3d5005c67d96ec73c5c4a37270f03d19
Keith Smiley <keithbsmiley@gmail.com>
2015-08-18 16:51:40 -0700

    Update SwiftLint for switch comments

--- Formula/swiftlint.rb
+++ Formula/swiftlint.rb
@@ -1,7 +1,7 @@
 class Swiftlint < Formula
   desc "Experimental tool to enforce Swift style and conventions"
   homepage "https://github.com/lyft/SwiftLint"
-  url "https://github.com/lyft/SwiftLint.git", :tag => "1.1.2", :revision => "528d105fda2b1364fc85681ede9fc846e6b43b24"
+  url "https://github.com/lyft/SwiftLint.git", :tag => "1.1.3", :revision => "7abd436991c349a32e3eadf410b8af70c82c3038"
   head "https://github.com/lyft/SwiftLint.git", :branch => "update-rules"
 
   depends_on :xcode => ["6.3", :build]
