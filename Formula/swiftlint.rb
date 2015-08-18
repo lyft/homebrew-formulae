75f1c2d4e81b1e3a789c66c5d026757e175d4a41
Keith Smiley <keithbsmiley@gmail.com>
2015-08-18 14:44:20 -0700

    Bump SwiftLint

--- Formula/swiftlint.rb
+++ Formula/swiftlint.rb
@@ -1,7 +1,7 @@
 class Swiftlint < Formula
   desc "Experimental tool to enforce Swift style and conventions"
   homepage "https://github.com/lyft/SwiftLint"
-  url "https://github.com/lyft/SwiftLint.git", :tag => "1.1.1", :revision => "54372d66285fd137be53ccc85d62c01d33d35478"
+  url "https://github.com/lyft/SwiftLint.git", :tag => "1.1.2", :revision => "528d105fda2b1364fc85681ede9fc846e6b43b24"
   head "https://github.com/lyft/SwiftLint.git", :branch => "update-rules"
 
   depends_on :xcode => ["6.3", :build]
