92d7e1b4597c527b6bc191f58c2e4b3382943e60
Keith Smiley <keithbsmiley@gmail.com>
2015-08-20 15:55:52 -0700

    Exclude TuneDelegate

--- Formula/swiftlint.rb
+++ Formula/swiftlint.rb
@@ -1,7 +1,7 @@
 class Swiftlint < Formula
   desc "Experimental tool to enforce Swift style and conventions"
   homepage "https://github.com/lyft/SwiftLint"
-  url "https://github.com/lyft/SwiftLint.git", :tag => "1.1.8", :revision => "3af781a53313558057d50466e51d1f8c67cec58b"
+  url "https://github.com/lyft/SwiftLint.git", :tag => "1.1.9", :revision => "91f25b1703f8f6cfcaab40265659f69112c5d6cd"
   head "https://github.com/lyft/SwiftLint.git", :branch => "update-rules"
 
   depends_on :xcode => ["6.3", :build]
