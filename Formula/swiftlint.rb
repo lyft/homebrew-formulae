3fd7512c185e11b1b651443a21cfdba29c8fb6a4
Keith Smiley <keithbsmiley@gmail.com>
2015-08-19 17:55:51 -0700

    Update swiftlint for blankline function rule

--- Formula/swiftlint.rb
+++ Formula/swiftlint.rb
@@ -1,7 +1,7 @@
 class Swiftlint < Formula
   desc "Experimental tool to enforce Swift style and conventions"
   homepage "https://github.com/lyft/SwiftLint"
-  url "https://github.com/lyft/SwiftLint.git", :tag => "1.1.5", :revision => "3316862e339d1257e1926983a0a15789e2cb5846"
+  url "https://github.com/lyft/SwiftLint.git", :tag => "1.1.6", :revision => "1a47c3ada3645cef1ba6a10c00d0f8f6d522af1d"
   head "https://github.com/lyft/SwiftLint.git", :branch => "update-rules"
 
   depends_on :xcode => ["6.3", :build]
