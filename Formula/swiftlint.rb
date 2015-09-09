61d170f9ed4231e73b6d227cf99219f67b9a0bb5
Keith Smiley <keithbsmiley@gmail.com>
2015-09-09 10:38:28 -0700

    Update swiftlint for Swift 2.0

--- Formula/swiftlint.rb
+++ Formula/swiftlint.rb
@@ -1,10 +1,10 @@
 class Swiftlint < Formula
   desc "Experimental tool to enforce Swift style and conventions"
   homepage "https://github.com/lyft/SwiftLint"
-  url "https://github.com/lyft/SwiftLint.git", :tag => "1.2.0", :revision => "cdcaf3139ff86ed5af43e107a785a4a50e6365d7"
-  head "https://github.com/lyft/SwiftLint.git", :branch => "update-rules"
+  url "https://github.com/lyft/SwiftLint.git", :tag => "2.0.0", :revision => "ff4e923b4158c89c9dbac71ab399d30b38330923"
+  head "https://github.com/lyft/SwiftLint.git", :branch => "swift-2.0-update-rules"
 
-  depends_on :xcode => ["6.3", :build]
+  depends_on :xcode => ["7.0", :build]
 
   def install
     system "make", "prefix_install", "PREFIX=#{prefix}", "TEMPORARY_FOLDER=#{buildpath}/SwiftLint.dst"
