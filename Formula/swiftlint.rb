b8105b8633d50cc02f9d3dc0daa6a0755b0e60c9
Martin Conte Mac Donell <martin@lyft.com>
2015-09-24 03:10:18 -0700

    Update SwiftLint with parenthesis regex match fix

--- Formula/swiftlint.rb
+++ Formula/swiftlint.rb
@@ -1,7 +1,7 @@
 class Swiftlint < Formula
   desc "Experimental tool to enforce Swift style and conventions"
   homepage "https://github.com/lyft/SwiftLint"
-  url "https://github.com/lyft/SwiftLint.git", :tag => "2.0.0", :revision => "ff4e923b4158c89c9dbac71ab399d30b38330923"
+  url "https://github.com/lyft/SwiftLint.git", :tag => "2.0.1", :revision => "1da015892fe7c68e0202c1f25b9b081b736b160f"
   head "https://github.com/lyft/SwiftLint.git", :branch => "swift-2.0-update-rules"
 
   depends_on :xcode => ["7.0", :build]
