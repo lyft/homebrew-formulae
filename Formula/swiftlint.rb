d4ff65863bfee8ecb975a80f0091e7d50a750bdd
Keith Smiley <keithbsmiley@gmail.com>
2015-10-21 13:50:37 -0700

    Update swiftlint to fix documentation rule missing

--- Formula/swiftlint.rb
+++ Formula/swiftlint.rb
@@ -1,7 +1,7 @@
 class Swiftlint < Formula
   desc "Experimental tool to enforce Swift style and conventions"
   homepage "https://github.com/lyft/SwiftLint"
-  url "https://github.com/lyft/SwiftLint.git", :tag => "2.0.3", :revision => "4c865317f8e4a83771c5a8e53e652cbd9c8ea699"
+  url "https://github.com/lyft/SwiftLint.git", :tag => "2.0.4", :revision => "9fba4ec4b08ceef933d5eed20e51d7087ae507d9"
   head "https://github.com/lyft/SwiftLint.git", :branch => "swift-2.0-update-rules"
 
   depends_on :xcode => ["7.0", :build]
