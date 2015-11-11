c6e08d7a82e93eace5e9dd7d71bd02c34ac494b7
Keith Smiley <keithbsmiley@gmail.com>
2015-11-10 21:03:47 -0800

    Bump swiftlint for $0 fix

--- Formula/swiftlint.rb
+++ Formula/swiftlint.rb
@@ -1,7 +1,7 @@
 class Swiftlint < Formula
   desc "Experimental tool to enforce Swift style and conventions"
   homepage "https://github.com/lyft/SwiftLint"
-  url "https://github.com/lyft/SwiftLint.git", :tag => "2.0.6", :revision => "4e6a0de4f384f6cf207e093070c6f6c2dffc2e44"
+  url "https://github.com/lyft/SwiftLint.git", :tag => "2.0.7", :revision => "11bd4cd9ce95b245e13225ea981dbc9c0386caf9"
   head "https://github.com/lyft/SwiftLint.git", :branch => "swift-2.0-update-rules"
 
   depends_on :xcode => ["7.0", :build]
