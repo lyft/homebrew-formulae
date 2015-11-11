d27cce25fac1d4b214ecd2e357ee234228fd275c
Martin Conte Mac Donell <martin@lyft.com>
2015-11-10 18:50:47 -0800

    Fix carthage and submodule

--- Formula/swiftlint.rb
+++ Formula/swiftlint.rb
@@ -1,7 +1,7 @@
 class Swiftlint < Formula
   desc "Experimental tool to enforce Swift style and conventions"
   homepage "https://github.com/lyft/SwiftLint"
-  url "https://github.com/lyft/SwiftLint.git", :tag => "2.0.5", :revision => "1540f765b8db9eecd0823beff83908ef904d6fc0"
+  url "https://github.com/lyft/SwiftLint.git", :tag => "2.0.6", :revision => "4e6a0de4f384f6cf207e093070c6f6c2dffc2e44"
   head "https://github.com/lyft/SwiftLint.git", :branch => "swift-2.0-update-rules"
 
   depends_on :xcode => ["7.0", :build]
