b91bec016745abb982856b28348a1fea7b241f66
Keith Smiley <keithbsmiley@gmail.com>
2017-12-28 11:56:51 -0800

    [set-simulator-location] 1.2.0 single simulator support

--- Formula/set-simulator-location.rb
+++ Formula/set-simulator-location.rb
@@ -1,8 +1,8 @@
 class SetSimulatorLocation < Formula
   homepage "https://github.com/lyft/set-simulator-location"
   head "https://github.com/lyft/set-simulator-location.git"
-  url "https://github.com/lyft/set-simulator-location/releases/download/1.1.0/set-simulator-location.tar.gz"
-  sha256 "6fe544b85eec698674fafbdb9c6d9845f85ec5e072af59a4da1d1ef022904e87"
+  url "https://github.com/lyft/set-simulator-location/releases/download/1.2.0/set-simulator-location.tar.gz"
+  sha256 "ccfba9168f2d9b3ebdf36fca98780f9aa70cd758d77b80f5e7e37fc216aa2f9f"
 
   def install
     if build.head?
