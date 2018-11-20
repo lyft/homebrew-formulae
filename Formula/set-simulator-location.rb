4fbdfe24834d0df780e48481481fe116fa7d3cd4
Keith Smiley <keithbsmiley@gmail.com>
2018-11-20 11:04:03 -0800

    [set-simulator-location] 1.4.0 Add udid support

--- Formula/set-simulator-location.rb
+++ Formula/set-simulator-location.rb
@@ -1,8 +1,8 @@
 class SetSimulatorLocation < Formula
   homepage "https://github.com/lyft/set-simulator-location"
   head "https://github.com/lyft/set-simulator-location.git"
-  url "https://github.com/lyft/set-simulator-location/releases/download/1.3.1/set-simulator-location.tar.gz"
-  sha256 "827d690d3217b46c83b78d313797bcee79a212a8e76670cf94702ee91b9d2a75"
+  url "https://github.com/lyft/set-simulator-location/releases/download/1.4.0/set-simulator-location.tar.gz"
+  sha256 "0339700371c0c6dad44a0e606b9bb6a3237126168410f5b716fcc53519d0c5c8"
 
   def install
     if build.head?
